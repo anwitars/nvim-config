62df622135338bde16ab8f4fef631e7080a4657b
Gábor Maksa <maksa22242@gmail.com>
2025-02-26 12:29:48 +0100

    anwitars: config: use rg instead builtin fuzzy

--- init.lua
+++ init.lua
@@ -403,11 +403,17 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
